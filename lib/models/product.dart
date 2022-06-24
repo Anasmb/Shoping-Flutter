eb8d2c37b9898879bb326f95e4fd242fce344759
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-06-24 16:29:33 +0300

    added product overview screen content added product item added theme and custom fonts

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Product {
   final String id;
@@ -6,7 +6,14 @@ class Product {
   final String description;
   final double price;
   final String imageUrl;
-  bool isFavorite; // not final because it is changeble 
+  bool isFavorite; // not final because it is changeble
 
-  Product({@required this.id , @required this.title , @required this.description, @required this.price , @required this.imageUrl,})
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
 }
